52ebd4c0455811712460552e718a9e0179c843e0
gracezhe <162688313+gracezhe@users.noreply.github.com>
2024-04-06 21:03:58 -0400

    commented out

--- draft 1.py
+++ draft 1.py
@@ -1,23 +1,23 @@
 import pygame
 from sys import exit
 
-pygame.init()
-screen = pygame.display.set_mode((800,400))
-pygame.display.set_caption('Learn Language')
-clock = pygame.time.Clock()
+# pygame.init()
+# screen = pygame.display.set_mode((800,400))
+# pygame.display.set_caption('Learn Language')
+# clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((800, 400))
-test_surface.fill('Red')
-second_test_surface = pygame.image.load('graphics/Sky.png') # location
+# # test_surface = pygame.Surface((800, 400))
+# # test_surface.fill('Red')
+# test_surface = pygame.image.load('Sky.jpg') # location
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-    screen.blit(test_surface, (0, 0))
-    pygame.display.update()
-    clock.tick(60)
+# while True:
+#     for event in pygame.event.get():
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#             exit()
+#     screen.blit(test_surface, (0, 0))
+#     pygame.display.update()
+#     clock.tick(60)
 
 
 print('hello')
