de7526fd2a0f422b70b346d537a2319c42f4d996
gracezhe <162688313+gracezhe@users.noreply.github.com>
2024-04-06 20:09:32 -0400

    basic set up

--- draft 1.py
+++ draft 1.py
@@ -1,7 +1,19 @@
 import pygame
+from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('Learn Language')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    # draw all our elements
+    # update
+    pygame.display.update()
+
 
 print('hello')
 print('alice')
