ef45575d9171080811144f062d1bd6df903d667d
gracezhe <162688313+gracezhe@users.noreply.github.com>
2024-04-06 20:15:01 -0400

    second surface

--- draft 1.py
+++ draft 1.py
@@ -4,15 +4,19 @@ from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Learn Language')
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((800, 400))
+test_surface.fill('Red')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    # draw all our elements
-    # update
+    screen.blit(test_surface, (0, 0))
     pygame.display.update()
+    clock.tick(60)
 
 
 print('hello')
